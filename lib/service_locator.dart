e59f5355af3f12351a86016dc7d62e6aa638086d
ender null <ender@end.works>
2023-06-23 12:31:34 +0200

    added cloudkit support

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:get_it/get_it.dart';
 import 'package:lovelust/services/common_service.dart';
 import 'package:lovelust/services/storage_service.dart';
+import 'package:lovelust/services/storage_service_cloudkit.dart';
 import 'package:lovelust/services/storage_service_local.dart';
 
 final getIt = GetIt.instance;
 
 setupServiceLocator() {
-  getIt.registerLazySingleton<StorageService>(() => StorageServiceLocal());
+  if (Platform.isIOS) {
+    getIt.registerLazySingleton<StorageService>(() => StorageServiceCloudKit());
+  } else {
+    getIt.registerLazySingleton<StorageService>(() => StorageServiceLocal());
+  }
   getIt.registerLazySingleton(() => CommonService());
 }

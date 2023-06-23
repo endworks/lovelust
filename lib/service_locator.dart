589b7e717d642aa930814773fa90c3ca3f5cd942
ender null <ender@end.works>
2023-06-23 12:49:08 +0200

    clean dependencies

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,18 +1,11 @@
-import 'dart:io';
-
 import 'package:get_it/get_it.dart';
 import 'package:lovelust/services/common_service.dart';
 import 'package:lovelust/services/storage_service.dart';
-import 'package:lovelust/services/storage_service_cloudkit.dart';
 import 'package:lovelust/services/storage_service_local.dart';
 
 final getIt = GetIt.instance;
 
 setupServiceLocator() {
-  if (Platform.isIOS) {
-    getIt.registerLazySingleton<StorageService>(() => StorageServiceCloudKit());
-  } else {
-    getIt.registerLazySingleton<StorageService>(() => StorageServiceLocal());
-  }
+  getIt.registerLazySingleton<StorageService>(() => StorageServiceLocal());
   getIt.registerLazySingleton(() => CommonService());
 }

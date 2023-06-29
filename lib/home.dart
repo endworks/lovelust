fed263c82c991a67540c4bedeaf4fb07d014b228
ender <ender@end.works>
2023-06-29 13:17:15 +0200

    Actualizar home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    common.initialLoad().whenComplete(() {
+    /* common.initialLoad().whenComplete(() {
       setState(() {});
       if (common.isLoggedIn()) {
         common.initialFetch().whenComplete(() {
@@ -29,7 +29,7 @@ class _HomeState extends State<Home> {
           // ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Initial fetch successful')));
         });
       }
-    });
+    });*/
   }
 
   @override

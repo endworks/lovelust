c60a41bf3d7975e37ef1fd5eac10067629eec9e1
ender <ender@end.works>
2023-07-29 09:03:12 +0200

    add ios widget

---
+++ ios/Status/StatusBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatusBundle.swift
+//  Status
+//
+//  Created by ender on 29/7/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatusBundle: WidgetBundle {
+    var body: some Widget {
+        Status()
+    }
+}
